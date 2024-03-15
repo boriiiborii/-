dfa9a4724283200562817d8564de95a5c2142243
Haena <ghkwlsspdl@naver.com>
2024-03-15 17:33:44 +0900

    hw02의 주석 처리

--- hw02/lab2-1.c
+++ hw02/lab2-1.c
@@ -5,9 +5,20 @@ int main()
       int integerType;
       float floatType;
       double doubleType;
-printf("Size of char: %ld byte\n",sizeof(charType)); printf("Size of int: %ld bytes\n",sizeof(integerType)); printf("Size of float: %ld bytes\n",sizeof(floatType)); printf("Size of double: %ld bytes\n",sizeof(doubleType));
+printf("Size of char: %ld byte\n",sizeof(charType)); //char 타입의 변수 사이즈는1
+printf("Size of int: %ld bytes\n",sizeof(integerType)); //int 타입의 변수 사이즈는4
+printf("Size of float: %ld bytes\n",sizeof(floatType));  //float 타입의 변수 사이즈는4
+printf("Size of double: %ld bytes\n",sizeof(doubleType)); //double 타입의 변수 사이즈는8
 printf("-----------------------------------------\n");
-printf("Size of char: %ld byte\n",sizeof(char)); printf("Size of int: %ld bytes\n",sizeof(int)); printf("Size of float: %ld bytes\n",sizeof(float)); printf("Size of double: %ld bytes\n",sizeof(double));
+printf("Size of char: %ld byte\n",sizeof(char)); //char 타입의 사이즈는1
+printf("Size of int: %ld bytes\n",sizeof(int)); //int 타입의 사이즈는4
+printf("Size of float: %ld bytes\n",sizeof(float));  //float 타입의 사이즈는4
+printf("Size of double: %ld bytes\n",sizeof(double)); //double 타입의 사이즈는8
 printf("-----------------------------------------\n");
-printf("Size of char*: %ld byte\n",sizeof(char*)); printf("Size of int*: %ld bytes\n",sizeof(int*)); printf("Size of float*: %ld bytes\n",sizeof(float*)); printf("Size of double*: %ld bytes\n",sizeof(double*));
+//주소는 현재 32비트 cpu의 주소체계를 쓰다보니 어떤 타입이던 주소값의 크기는 동일하게 8바이트인것을 확인할 수 있다.
+//주소값의 길이는 타입과 상관없이 몇비트의 cpu를 쓰느냐에 따라서 달라진다. (32비트는 8바이트)
+printf("Size of char*: %ld byte\n",sizeof(char*)); 
+printf("Size of int*: %ld bytes\n",sizeof(int*)); 
+printf("Size of float*: %ld bytes\n",sizeof(float*)); 
+printf("Size of double*: %ld bytes\n",sizeof(double*));
 return 0; }
--- hw02/lab2-2.c
+++ hw02/lab2-2.c
@@ -6,34 +6,46 @@ int **dptr;
 i = 1234;
 char yourName[] = "이화진";
 char studentID[] = "2020037103";
-printf("[----- [%s]  [%s] -----]\n\n", yourName, studentID);
-printf("[checking values before ptr = &i] \n"); 
-printf("value of i == %d\n", i); 
-printf("address of i == %p\n", &i); 
-printf("value of ptr == %p\n", ptr); 
-printf("address of ptr == %p\n", &ptr);
+
+printf("[----- [%s]  [%s] -----]\n\n", yourName, studentID);// 단순출력
+printf("[checking values before ptr = &i] \n"); //단순출력
+printf("value of i == %d\n", i); // i의 밸류는 1234
+printf("address of i == %p\n", &i); // i의 주소는 3260
+printf("value of ptr == %p\n", ptr); // ptr의 값은 쓰레기값(값 세팅 전이라)
+printf("address of ptr == %p\n", &ptr);// ptr의 주소는 3258로 지정됨
+
 ptr = &i; /* ptr is now holding the address of i */
-
 printf("\n[checking values after ptr = &i] \n"); 
-printf("value of i == %d\n", i); 
-printf("address of i == %p\n", &i); 
-printf("value of ptr == %p\n", ptr); 
-printf("address of ptr == %p\n", &ptr); 
-printf("value of *ptr == %d\n", *ptr);
+printf("value of i == %d\n", i); // i의 밸류는 그대로
+printf("address of i == %p\n", &i); // 역시 id의 주소도 그대로
+printf("value of ptr == %p\n", ptr); // ptr에 값을 i의 주소로 넣었으니 3260
+printf("address of ptr == %p\n", &ptr); // ptr주소 역시 여전함
+printf("value of *ptr == %d\n", *ptr); // ptr밸류의 값을 참조 > 주소 3260의값 > 1234
+
 dptr = &ptr; /* dptr is now holding the address of ptr */
+printf("\n[checking values after dptr = &ptr] \n"); 
+printf("value of i == %d\n", i); //위와 동일
+printf("address of i == %p\n", &i); //위와 동일
+printf("value of ptr == %p\n", ptr); //위와 동일
+printf("address of ptr == %p\n", &ptr); //위와 동일
+printf("value of *ptr == %d\n", *ptr); //위와 동일
+printf("value of dptr == %p\n", dptr); // dptr의 값은 ptr의 주소 3258
+printf("address of dptr == %p\n", &dptr); //dptr의 주소 3250
+printf("value of *dptr == %p\n", *dptr); // ptr의 값을 출력(i의 주소) 3260
+printf("value of **dptr == %d\n", **dptr); // i의 값을 출력 1234
 
-printf("\n[checking values after dptr = &ptr] \n"); printf("value of i == %d\n", i);
-printf("address of i == %p\n", &i);
-printf("value of ptr == %p\n", ptr); printf("address of ptr == %p\n", &ptr); printf("value of *ptr == %d\n", *ptr); printf("value of dptr == %p\n", dptr); printf("address of dptr == %p\n", &dptr); printf("value of *dptr == %p\n", *dptr);
-printf("value of **dptr == %d\n", **dptr);
 *ptr = 7777; /* changing the value of *ptr */
-
 printf("\n[after *ptr = 777] \n");
-printf("value of i == %d\n", i); printf("value of *ptr == %d\n", *ptr);
-printf("value of **dptr == %d\n", **dptr);
-**dptr = 8888; /* changing the value of **dptr */
+//i의 값이나 *ptr의 값이나 **dptr의 값은 같은 메모리 공간을 사용중이다.
+printf("value of i == %d\n", i); // 7777
+printf("value of *ptr == %d\n", *ptr); // 7777
+printf("value of **dptr == %d\n", **dptr); // 7777
 
+**dptr = 8888; /* changing the value of **dptr */
+//i의 값이나 *ptr의 값이나 **dptr의 값은 같은 메모리 공간을 사용중이다.
 printf("\n[after **dptr = 8888] \n");
-printf("value of i == %d\n", i); printf("value of *ptr == %d\n", *ptr); printf("value of **dptr == %d\n", **dptr);
+printf("value of i == %d\n", i); // 8888
+printf("value of *ptr == %d\n", *ptr); // 8888
+printf("value of **dptr == %d\n", **dptr);// 8888
 return 0;
 }
